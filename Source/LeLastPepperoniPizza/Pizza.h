f49251fe6b5b0afff1cfb8e304ae1651d74067c7
OshiroDrey <minhtoan2746@gmail.com>
2025-08-15 10:26:22 +0700

    add pizza assets and placeholders

---
+++ Source/LeLastPepperoniPizza/Pizza.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BasePawn.h"
+#include "Pizza.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LELASTPEPPERONIPIZZA_API APizza : public ABasePawn
+{
+	GENERATED_BODY()
+	
+};
