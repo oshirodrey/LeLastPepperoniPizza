017da774b778025e02bfde6fc8a91f1f287e7d72
OshiroDrey <minhtoan2746@gmail.com>
2025-08-20 09:21:17 +0700

    change the menu background
    the last one was not met my eyes even if its just temporary

---
+++ Source/LeLastPepperoniPizza/LeLastPepperoniPizzaGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "LeLastPepperoniPizzaGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LELASTPEPPERONIPIZZA_API ALeLastPepperoniPizzaGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
