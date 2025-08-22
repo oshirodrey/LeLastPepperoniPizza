e4300543e38d57793ad3ce5da9fda2e7bbd8b6e3
OshiroDrey <minhtoan2746@gmail.com>
2025-08-22 12:03:38 +0700

    Spawn enemy randomly in interval

--- Source/LeLastPepperoniPizza/LeLastPepperoniPizzaGameMode.cpp
+++ Source/LeLastPepperoniPizza/LeLastPepperoniPizzaGameMode.cpp
@@ -2,4 +2,33 @@
 
 
 #include "LeLastPepperoniPizzaGameMode.h"
+#include "Pineapple.h"
+#include "TimerManager.h"
+#include "Kismet/GameplayStatics.h"
 
+
+void ALeLastPepperoniPizzaGameMode::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// Spawn enemies every after set interval
+		GetWorld()->GetTimerManager().SetTimer(SpawnTimerHandle,
+			this,
+			&ALeLastPepperoniPizzaGameMode::RandomlySpawnEnemyInRadius,
+			SpawnInterval,
+			true);
+	
+	
+}
+
+void ALeLastPepperoniPizzaGameMode::RandomlySpawnEnemyInRadius()
+{
+	float Angle = FMath::FRandRange(0, 2*PI);
+	float XLocation = FMath::Cos(Angle) * SpawnRadius;
+	float YLocation = FMath::Sin(Angle) * SpawnRadius;
+	FVector SpawnLocation = FVector(XLocation, YLocation, 68);
+	APineapple* NewPineapple = GetWorld()->SpawnActor<APineapple>(PineappleClass, SpawnLocation, FRotator(0, 0, 0));
+	
+	
+	
+}
--- Source/LeLastPepperoniPizza/LeLastPepperoniPizzaGameMode.h
+++ Source/LeLastPepperoniPizza/LeLastPepperoniPizzaGameMode.h
@@ -13,5 +13,20 @@ UCLASS()
 class LELASTPEPPERONIPIZZA_API ALeLastPepperoniPizzaGameMode : public AGameModeBase
 {
 	GENERATED_BODY()
+protected:
+	virtual void BeginPlay() override;
+
+private:
+	void RandomlySpawnEnemyInRadius();
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, BlueprintReadWrite, meta=(AllowPrivateAccess="true"), Category = "Properties")
+	float SpawnRadius = 400.f;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, BlueprintReadWrite, meta=(AllowPrivateAccess="true"), Category = "Properties")
+	float SpawnInterval = 3.f;
+	FTimerHandle SpawnTimerHandle;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, BlueprintReadWrite, meta=(AllowPrivateAccess="true"), Category = "Properties")
+	TSubclassOf<class APineapple> PineappleClass;
+
+	class APineapple* Pineapple;
 	
 };
